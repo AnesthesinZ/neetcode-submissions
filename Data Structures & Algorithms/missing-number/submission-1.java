69bc7a164a2a900704f8e51917c2eab05aa193e9
Sean <shengqiao.zhao@mail.utoronto.ca>
2026-05-27 16:00:10 -0400

    Add: missing-number - submission-1

---
+++ Data Structures & Algorithms/missing-number/submission-1.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int mask = 0;
+        for (int i = 0; i < nums.length; i++) {
+            mask ^= i;
+            mask ^= nums[i];
+        }
+
+        mask ^= nums.length;
+
+        return mask;
+    }
+}
